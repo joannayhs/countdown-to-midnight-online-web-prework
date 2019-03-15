369d1d8191eb43e2015fd02a2522a0bed497bfeb
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-15 22:18:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,12 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  
+def countdown_with_sleep(number)
+  number = 10
+  while number >10
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep 1
+end
+"HAPPY NEW YEAR!"
 end
