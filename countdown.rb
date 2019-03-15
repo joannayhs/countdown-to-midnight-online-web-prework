8473cf5da619775f8910399d5cecd039c0e45272
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-15 21:56:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  number = 10
+  while number > 0
+  number -= 1
+    puts "#{number} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  
 end
